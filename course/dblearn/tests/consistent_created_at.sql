fa4999e5fcb55406fcbdafe856582bc473389a9f
SriramAvatar <sriramn84@gmail.com>
2025-05-02 15:51:15 +0000

    add macros

--- course/dblearn/tests/consistent_created_at.sql
+++ course/dblearn/tests/consistent_created_at.sql
@@ -1,4 +1,4 @@
 SELECT * FROM {{ ref('dim_listings_cleansed') }} l
 INNER JOIN {{ ref('fct_reviews') }} r
-using (listing_id)
- where l.created_at >= r.created_at
+USING (listing_id)
+WHERE l.created_at >= r.review_date
